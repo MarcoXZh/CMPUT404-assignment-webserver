1b93f067f8667ae62793c63602133cdbe66ca389
MarcoXZh <zxu3@ualberta.ca>
2014-01-17 10:52:03 -0700

    Coding finished

--- server.py
+++ server.py
@@ -17,7 +17,7 @@ import SocketServer
 #
 #
 # Furthermore it is derived from the Python documentation examples thus
-# some of the code is Copyright © 2001-2013 Python Software
+# some of the code is Copyright Â© 2001-2013 Python Software
 # Foundation; All Rights Reserved
 #
 # http://docs.python.org/2/library/socketserver.html
@@ -26,13 +26,54 @@ import SocketServer
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+import os
 
 class MyWebServer(SocketServer.BaseRequestHandler):
-    
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall("OK")
+        path = self.data.split(" ")[1]
+        fullpath = os.path.normpath(os.getcwd() + "/www" + path)
+        try:
+            if not fullpath.startswith(os.getcwd() + "/www"):
+                raise IOError
+            elif not os.path.exists(fullpath):
+                self.respond(404, None);
+            elif os.path.isdir(fullpath) and path[-1] != "/":
+                self.respond(301, path);
+            else:
+                if os.path.isdir(fullpath):
+                    fullpath += "/index.html"
+                self.respond(200, fullpath);
+        except IOError:
+            self.respond(404, None);
+    pass # def handle(self)
+
+    def respond(self, code, url):
+        msg = ""
+        if code == 404:
+            msg = "HTTP/1.1 404 Not Found\n"
+            msg += "Content-Type: text/html; charset=UTF-8\n\n"
+            msg += "PAGE NOT FOUND\n"
+        elif code == 301:
+            msg = "HTTP/1.1 301 Moved Permanently\n"
+            msg += "Location: " + url + "/\n\n"
+            msg += "PAGE HAS BEEN MOVED PERMANENTLY\n"
+        elif code == 200:
+            msg = "HTTP/1.1 200 OK\n"
+            if url.endswith(".html"):
+                msg += "Content-Type: text/html; charset=UTF-8\n\n"
+            elif url.endswith(".css"):
+                msg += "Content-Type: text/css; charset=UTF-8\n\n"
+            else:
+                msg += "Content-Type: text/plain; charset=UTF-8\n\n"                
+            file = open(url, "r")
+            msg += file.read()
+            file.close()
+        self.request.sendall(msg)
+    pass # def respond(self, code, url)
+
+pass # class MyWebServer(SocketServer.BaseRequestHandler)
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
@@ -44,3 +85,5 @@ if __name__ == "__main__":
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+pass # if __name__ == "__main__"
+
